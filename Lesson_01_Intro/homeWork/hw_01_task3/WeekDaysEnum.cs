afc76231c62e97072bb10a53d42c854484acd01c
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-03 22:42:57 +0300

    Seminar#1 home work task#3 done

---
+++ Lesson_01_Intro/homeWork/hw_01_task3/WeekDaysEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_01_task3
+{
+    internal enum WeekDaysEnum
+    {
+        Monday = 1
+        ,Tuesday
+        ,Wednesday
+        ,Thursday
+        ,Friday
+        ,Saturday
+        ,Sunday
+    }
+}
