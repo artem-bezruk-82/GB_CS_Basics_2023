12e1bf2e9e8f9fe930deabf3ac1838ea9a70e22a
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-04 20:08:59 +0300

    Seminar#2 class work task#9 done

---
+++ Lesson_02_Intro/classWork/cw_02_task9/Program.cs
@@ -0,0 +1,18 @@
+﻿//Семинар #2
+//Задача #9
+//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+
+namespace cw_02_task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            byte value = (byte)random.Next(10, 99);
+            byte maxDigit = (value % 10 > value / 10) ? (byte)(value % 10) : (byte)(value / 10);
+            Console.WriteLine($"Value: {value}; MaxDigit: {maxDigit} ");
+        }
+    }
+}
