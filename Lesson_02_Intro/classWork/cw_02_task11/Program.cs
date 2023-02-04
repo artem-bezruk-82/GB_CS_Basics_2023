1dc72a306ccb66232c02f5a11d2608d9e08e003f
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-04 23:01:31 +0300

    Seminar#2 class work task#11 done

---
+++ Lesson_02_Intro/classWork/cw_02_task11/Program.cs
@@ -0,0 +1,16 @@
+﻿//Семинар #2
+//Задача #11
+//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+namespace cw_02_task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int value = random.Next(100,999);
+            Console.WriteLine($"Value: {value}; 2nd digit:{(value / 10) % 10}");
+        }
+    }
+}
