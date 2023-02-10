fac89ec2e0d46d59841e94429a52069ea0519424
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-10 21:02:08 +0300

    Seminar#3 home work task#21 done

---
+++ Lesson_03_Intro/homeWork/hw_03_task21/Program.cs
@@ -0,0 +1,25 @@
+﻿//Семинар #3
+//Задача #21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+namespace hw_03_task21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Point3D pointA = new Point3D();
+            Console.WriteLine($"Create {nameof(pointA)}");
+            pointA.SetPointConsole();
+
+            Point3D pointB = new Point3D();
+            Console.WriteLine($"Create {nameof(pointB)}");
+            pointB.SetPointConsole();
+
+            Console.WriteLine($"{nameof(pointA)}: {pointA}\n{nameof(pointB)}: {pointB}");
+
+            Console.WriteLine($"Distance: {Math.Round(Point3D.GetDistance(pointA, pointB),2)}");
+        }
+    }
+}
