9cfe1705e4455fcfcbc06ee2ef952652df227345
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-11 22:58:05 +0300

    Seminar#4 class work task#26 done

---
+++ Lesson_04_Functions/classWork/cw_04_task26/Program.cs
@@ -0,0 +1,22 @@
+﻿//Семинар #4
+//Задача #26
+//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+namespace cw_04_task26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter value");
+            int value = Convert.ToInt32(Console.ReadLine());
+            int counter = 0;
+            while (value != 0) 
+            {
+                value /= 10;
+                counter++;
+            }
+            Console.WriteLine($"You have entered {counter}-digits value");
+        }
+    }
+}
