140a21f3f74b10882f6e5496fc2add73a5c7ca2e
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-11 21:59:24 +0300

    Seminar#4 home work task#25 done

---
+++ Lesson_04_Functions/homeWork/hw_04_task25/Program.cs
@@ -0,0 +1,50 @@
+﻿//Семинар #4
+//Задача #25
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//Нельзя использовать Math.Pow();
+
+namespace hw_04_task25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = GetConsoleInput($"Please enter {nameof(value)} ({typeof(int).Name})");
+            int power = GetConsoleInput($"Please enter {nameof(power)} ({typeof(int).Name})");
+
+            double result = value;
+
+            for (int i = 1; i < Math.Abs(power); i++)
+            {
+                result *= value;
+            }
+
+            if (power < 0)
+            {
+                result = 1 / result;
+            }
+
+            Console.WriteLine($"{value}^{power} = {result}");
+        }
+
+        static int GetConsoleInput(string requestText)
+        {
+            int? value = null;
+
+            while (value is null)
+            {
+                Console.WriteLine(requestText);
+                try
+                {
+                    value = int.Parse(Console.ReadLine() ?? "");
+                }
+                catch (Exception exc)
+                {
+
+                    Console.WriteLine($"{exc.Message} Please enter {typeof(int).Name} value");
+                }
+            }
+            return (int)value;
+        }
+    }
+}
