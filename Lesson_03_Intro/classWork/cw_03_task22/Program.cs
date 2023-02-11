b96466fd7d4b0207e161f4baa3b05a44bf10beb1
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-11 20:30:57 +0300

    Seminar#3 class work task#22 done

---
+++ Lesson_03_Intro/classWork/cw_03_task22/Program.cs
@@ -0,0 +1,38 @@
+﻿//Семинар #3
+//Задача #22
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+namespace cw_03_task22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = Math.Abs(GetConsoleInput($"Please enter {nameof(value)}"));
+            for (int i = 1; i <= value; i++)
+            {
+                Console.Write($" {Math.Pow(i, 2)}");
+            }
+        }
+
+        static int GetConsoleInput(string requestText)
+        {
+            int? value = null;
+
+            while (value is null)
+            {
+                Console.WriteLine(requestText);
+                try
+                {
+                    value = int.Parse(Console.ReadLine() ?? "");
+                }
+                catch (Exception exc)
+                {
+
+                    Console.WriteLine($"{exc.Message} Please enter integer value");
+                }
+            }
+            return (int)value;
+        }
+    }
+}
