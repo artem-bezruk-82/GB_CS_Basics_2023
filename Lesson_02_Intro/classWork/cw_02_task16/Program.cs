1f582414ccbe5b9cddad04bb903d19bc337de795
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-11 12:59:25 +0300

    Seminar#2 class work task#16 done

---
+++ Lesson_02_Intro/classWork/cw_02_task16/Program.cs
@@ -0,0 +1,45 @@
+﻿//Семинар #2
+//Задача #16
+//Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+namespace cw_02_task16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int valueA = GetConsoleInput($"Please enter {nameof(valueA)}");
+            int valueB = GetConsoleInput($"Please enter {nameof(valueB)}");
+
+            if (valueA > valueB)
+            {
+                (valueA,valueB)=(valueB,valueA);
+            }
+
+            string sign = (Math.Pow(valueA, 2) == valueB) ? "=" : "!=";
+            Console.WriteLine($"{valueA}^2 {sign} {valueB}");
+
+        }
+
+        static int GetConsoleInput(string requestText)
+        {
+            int? value = null;
+
+            while (value is null)
+            {
+                Console.WriteLine(requestText);
+                try
+                {
+                    value = int.Parse(Console.ReadLine() ?? "");
+                }
+                catch (Exception exc)
+                {
+
+                    Console.WriteLine($"{exc.Message} Please enter {typeof(int).Name} value");
+                }
+            }
+            return (int)value;
+        }
+    }
+}
