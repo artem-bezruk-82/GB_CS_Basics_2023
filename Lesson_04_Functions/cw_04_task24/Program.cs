4abe706e2290a30c5a6b179c0840df94f729dfed
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-11 20:46:51 +0300

    Seminar#4 class work task#24 done

---
+++ Lesson_04_Functions/cw_04_task24/Program.cs
@@ -0,0 +1,41 @@
+﻿//Семинар #4
+//Задача #24
+//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+namespace cw_04_task24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int value = GetConsoleInput($"Please enter {nameof(value)} ({typeof(int).Name})");
+            int sum = 0;
+            for (int i = 0; i <= value; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+
+        static int GetConsoleInput(string requestText)
+        {
+            int? value = null;
+
+            while (value is null)
+            {
+                Console.WriteLine(requestText);
+                try
+                {
+                    value = int.Parse(Console.ReadLine() ?? "");
+                }
+                catch (Exception exc)
+                {
+
+                    Console.WriteLine($"{exc.Message} Please enter {typeof(int).Name} value");
+                }
+            }
+            return (int)value;
+        }
+    }
+}
