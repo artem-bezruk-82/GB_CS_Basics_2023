294a38804dd8e85ab69f55b136a3d538c6666aa9
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-11 23:33:28 +0300

    Seminar#4 home work task#27 done

---
+++ Lesson_04_Functions/homeWork/hw_04_task27/Program.cs
@@ -0,0 +1,44 @@
+﻿//Семинар #4
+//Задача #27
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+namespace hw_04_task27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = GetConsoleInput($"Please enter {nameof(value)} ({typeof(int).Name})");
+            int sum = 0;
+
+            while (value != 0)
+            {
+                sum += value % 10;
+                value /= 10;
+            }
+
+            Console.WriteLine(Math.Abs(sum));
+        }
+
+        static int GetConsoleInput(string requestText)
+        {
+            int? value = null;
+
+            while (value is null)
+            {
+                Console.WriteLine(requestText);
+                try
+                {
+                    value = int.Parse(Console.ReadLine() ?? "");
+                }
+                catch (Exception exc)
+                {
+
+                    Console.WriteLine($"{exc.Message} Please enter {typeof(int).Name} value");
+                }
+            }
+            return (int)value;
+        }
+    }
+}
