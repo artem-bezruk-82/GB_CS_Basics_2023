32b76509ec1e1ce36f239aa582232c922cc19c47
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-12 20:32:22 +0300

    Semimar#5 home work task#36 done

---
+++ Lessson_05_Arrays/homeWork/hw_05_task36/Program.cs
@@ -0,0 +1,33 @@
+﻿//Семинар #5
+//Задача #36
+//Задайте одномерный массив, заполненный случайными числами.
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+namespace hw_05_task36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] arr = new int[random.Next(5,26)];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(1,100);
+            }
+
+            int sumOfEvenValues = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sumOfEvenValues += arr[i];
+                }
+            }
+
+            Console.WriteLine($"{nameof(sumOfEvenValues)} = {sumOfEvenValues}");
+        }
+    }
+}
