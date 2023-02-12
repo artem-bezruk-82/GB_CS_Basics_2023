46b22b1394f2b4d5b89f20381e76920fd3f2e57f
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-12 11:37:17 +0300

    Seminar#4 class work task#30 done

---
+++ Lesson_04_Functions/classWork/cw_04_task30/Program.cs
@@ -0,0 +1,21 @@
+﻿//Семинар #4
+//Задача #30
+//Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+
+namespace cw_04_task30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[8];
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(0,2);
+            }
+            Console.WriteLine($"[{string.Join(',', arr)}]");
+        }
+    }
+}
