50911d05d0b054cc6e6f68d65f4502156e4ab160
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-12 15:09:50 +0300

    Semimar#5 class work task#32 done

---
+++ Lessson_05_Arrays/classWork/cw_05_task32/Program.cs
@@ -0,0 +1,29 @@
+﻿//Семинар #5
+//Задача #32
+//Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+namespace cw_05_task32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] arr = new int[random.Next(1,15)];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(-100, 101);
+            }
+
+            Console.WriteLine($"[{string.Join(',', arr)}]");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] *= -1; 
+            }
+
+            Console.WriteLine($"[{string.Join(',', arr)}]");
+        }
+    }
+}
