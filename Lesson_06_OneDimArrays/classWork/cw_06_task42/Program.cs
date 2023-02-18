2fddbb079f6b32a9617ba446acfb26e97644ade7
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-18 18:19:59 +0300

    Seminar#06 class work task 42 done

---
+++ Lesson_06_OneDimArrays/classWork/cw_06_task42/Program.cs
@@ -0,0 +1,43 @@
+﻿//Семинар #6
+//Задача #42
+//Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+namespace cw_06_task42
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valueDec = GetConsoleInput($"Please enter {typeof(int).Name} value");
+            string binary = string.Empty;
+
+            while (valueDec > 0) 
+            {
+                binary = $"{valueDec % 2}{binary}";
+                valueDec /= 2;
+            }
+
+            Console.WriteLine(binary);
+        }
+
+        static int GetConsoleInput(string requestText)
+        {
+            int? value = null;
+
+            while (value is null)
+            {
+                Console.WriteLine(requestText);
+                try
+                {
+                    value = int.Parse(Console.ReadLine() ?? "");
+                }
+                catch (Exception exc)
+                {
+
+                    Console.WriteLine($"{exc.Message} Please enter integer value");
+                }
+            }
+            return (int)value;
+        }
+    }
+}
