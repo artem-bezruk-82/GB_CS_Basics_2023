6864eba70a1034a654a2453dac5fafa41cc9a067
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-18 08:45:08 +0300

    Seminar#06 class work task 39 done

---
+++ Lesson_06_OneDimArrays/classWork/cw_06_task39/Program.cs
@@ -0,0 +1,35 @@
+﻿//Семинар #6
+//Задача #39
+//Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+namespace cw_06_task39
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int[] arr = new int[random.Next(2,15)];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(100);
+            }
+
+            Console.WriteLine(string.Join(' ',arr));
+
+            Reverse1DArray(arr);
+
+            Console.WriteLine(string.Join(' ', arr));
+        }
+
+        static void Reverse1DArray(int[] array) 
+        {
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                (array[i], array[array.Length - i - 1]) =(array[array.Length - i - 1], array[i]);
+            }
+        }
+    }
+}
