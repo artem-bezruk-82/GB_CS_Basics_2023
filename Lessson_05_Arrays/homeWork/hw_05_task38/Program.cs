fc4c637e9d63dcd73115193e128f3d21f53c0e46
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-18 06:21:31 +0300

    Seminar#5 home work task#38 done

---
+++ Lessson_05_Arrays/homeWork/hw_05_task38/Program.cs
@@ -0,0 +1,28 @@
+﻿//Семинар #5
+//Задача #38
+//Задайте массив вещественных чисел.
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+
+namespace hw_05_task38
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = new double[new Random().Next(3,15)];
+
+            Random random = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Math.Round(random.NextDouble() * 100, 2);
+            }
+            Console.WriteLine($"{string.Join(' ', arr)}");
+
+            Array.Sort(arr);
+
+            Console.WriteLine($"{arr[arr.Length - 1]} - {arr[0]} = {arr[arr.Length - 1] - arr[0]}");
+        }
+    }
+}
