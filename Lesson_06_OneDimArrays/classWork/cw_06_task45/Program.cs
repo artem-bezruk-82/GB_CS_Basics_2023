f6da4b7e6a7c53902c19fc0f36d9564427959b75
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-19 12:54:01 +0300

    Seminar#06 class work task#45 done

---
+++ Lesson_06_OneDimArrays/classWork/cw_06_task45/Program.cs
@@ -0,0 +1,41 @@
+﻿//Семинар #6
+//Задача #45
+//Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+namespace cw_06_task45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] arrA = new int[random.Next(15)];
+
+            for (int i = 0; i < arrA.Length; i++)
+            {
+                arrA[i] = random.Next(100);
+            }
+
+            int[] arrB = CopyArr(arrA);
+
+            Console.WriteLine("Initial array:");
+            Console.WriteLine($"[{string.Join(' ', arrA)}]");
+
+            Console.WriteLine("Copy array:");
+            Console.WriteLine($"[{string.Join(' ', arrB)}]");
+        }
+
+
+        static int[] CopyArr(int[] array) 
+        {
+            int[] copy = new int[array.Length];
+
+            for (int i = 0; i < copy.Length; i++)
+            {
+                copy[i] = array[i];
+            }
+
+            return copy;
+        }
+    }
+}
