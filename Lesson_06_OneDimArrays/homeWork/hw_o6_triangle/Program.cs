fcb9d9325f015eaf1f03ecc20a143f8afdaf5e16
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-19 08:09:21 +0300

    Seminar#06 home work task Triangle done

---
+++ Lesson_06_OneDimArrays/homeWork/hw_o6_triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿//Семинар #6
+//Дополнительная задача
+//Площадь треугольника(https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=6&id_topic=116&id_problem=719)
+//По целочисленным координатам вершин треугольника (x1,y1), (x2,y2) и (x3,y3) требуется вычислить его площадь.
+//Входные данные
+//6 целых чисел x1, y1, x2, y2, x3, y3 – координаты вершин треугольника.
+//Все числа не превышают 106 по абсолютной величине.
+//Выходные данные
+//Выведите точное значение площади заданного треугольника.
+
+namespace hw_o6_triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random= new Random();
+
+            Point2D point1 = new Point2D(random.Next(107), random.Next(107));
+            Point2D point2 = new Point2D(random.Next(107), random.Next(107));
+            Point2D point3 = new Point2D(random.Next(107), random.Next(107));
+
+            Console.WriteLine($"{nameof(point1)}: {point1}\n{nameof(point2)}: {point2}\n{nameof(point3)}: {point3}");
+
+            double sideA = Point2D.GetDistance(point1, point2);
+            double sideB = Point2D.GetDistance(point1, point3);
+            double sideC = Point2D.GetDistance(point3, point2);
+
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+            Console.WriteLine($"{nameof(triangle)}'s sides: {triangle}");
+            Console.WriteLine($"Squere: {Math.Round(triangle.GetSquare(), 2)}");
+        }
+    }
+}
---
+++ Lesson_06_OneDimArrays/homeWork/hw_o6_triangle/Triangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_o6_triangle
+{
+    internal class Triangle
+    {
+        public double SideA { get; private set; }
+        public double SideB { get; private set; }
+        public double SideC { get; private set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (IsTriangle(sideA, sideB, sideC))
+            {
+                SideA = sideA; SideB = sideB; SideC = sideC;
+            }
+            else
+            {
+                throw new ArgumentException(message: "Triangle does not fit triangle inequality theorem rule");
+            }
+        }
+
+        public static bool IsTriangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA < sideB + sideC && sideB < sideA + sideC && sideC < sideA + sideB)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public double GetPerimeter() 
+        {
+            return SideA + SideB + SideC;
+        }
+
+        public double GetSquare() 
+        {
+            double p = GetPerimeter() / 2;
+            return Math.Sqrt(p*(p - SideA)* (p - SideB)* (p - SideC));
+        }
+
+        public override string ToString()
+        {
+            return $"[{SideA}; {SideB}; {SideC}]";
+        }
+    }
+}
