52181151d09df9f969b04195081aa738fb50cc22
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-19 12:38:32 +0300

    Seminar#06 class work task#44 done

---
+++ Lesson_06_OneDimArrays/classWork/cw_06_task44/Program.cs
@@ -0,0 +1,30 @@
+﻿//Семинар #6
+//Задача #44
+//Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+namespace cw_06_task44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+            int first = 0;
+            int second = 1;
+            int result = 0;
+
+            Console.Write($"{first} {second} ");
+
+            while (second < n)
+            {
+                result = first + second;
+                Console.Write($"{result} ");
+
+                first = second;
+                second = result;
+            };
+
+
+        }
+    }
+}
