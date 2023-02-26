a5760edb088074551d36f2d9a4fc0c08cc737c56
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-26 08:40:12 +0300

    Seminar#08 home work task#61 done

---
+++ Lesson_08_TwoDimArrays/homeWork/hw_08_task61/Program.cs
@@ -0,0 +1,45 @@
+﻿//Семинар #8
+//Задача #61
+//Вывести первые N строк треугольника Паскаля.
+//Сделать вывод в виде равнобедренного треугольника
+
+namespace hw_08_task61
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int rows = 5;
+            PrintTriangleConsole(rows);
+        }
+
+        static int GetFactorial(int value)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= value; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+
+        static void PrintTriangleConsole(int rows)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j <= (rows - i); j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" ");
+                    Console.Write(GetFactorial(i) / (GetFactorial(j) * GetFactorial(i - j)));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
