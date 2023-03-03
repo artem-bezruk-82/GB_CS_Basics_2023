458b3408a70159a62fdfc2fb975025bc8c1d246c
Artem Bezruk <artem.bezruk@gmail.com>
2023-03-03 20:39:48 +0300

    Seminar#9 home work task#66 done

---
+++ Lesson_09_Recursion/homeWork/hw_09_task66/Program.cs
@@ -0,0 +1,50 @@
+﻿//Семинар #9
+//Задача #66
+//Задайте значения M и N.
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+namespace hw_09_task66
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = GetConsoleInput($"Please enter {nameof(start)} value ({typeof(int)})");
+            int end = GetConsoleInput($"Please enter {nameof(end)} value ({typeof(int)})");
+            Console.WriteLine(GetSumInRangeRec(start, end));
+        }
+
+        static int GetSumInRangeRec(int startRange, int endRange) 
+        {
+            if (startRange == endRange) 
+            {
+                return endRange; 
+            }
+            if (startRange > endRange) 
+            {
+                (startRange, endRange) = (endRange, startRange);
+            }
+            return GetSumInRangeRec(startRange + 1, endRange) + startRange;
+        }
+
+        static int GetConsoleInput(string requestText)
+        {
+            int? value = null;
+
+            while (value is null)
+            {
+                Console.WriteLine(requestText);
+                try
+                {
+                    value = int.Parse(Console.ReadLine() ?? "");
+                }
+                catch (Exception exc)
+                {
+
+                    Console.WriteLine($"{exc.Message} Please enter {typeof(int)} type value");
+                }
+            }
+            return (int)value;
+        }
+    }
+}
